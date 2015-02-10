3cdf359e7274b5ce7da8b434efccb5e76ff5cc6e
Roberto Rodrigues Junior <robertogyn19@gmail.com>
2015-02-10 16:34:14 -0200

    Fix indentation

--- twitter_entities.go
+++ twitter_entities.go
@@ -1,56 +1,56 @@
 package anaconda
 
 type UrlEntity struct {
-	display_url		string	`json:"display_url"`
-	Expanded_url	string	`json:"expanded_url"`
-	Indices				[]int		`json:"indices"`
-	Url						string	`json:"url"`
+	display_url   string  `json:"display_url"`
+	Expanded_url  string  `json:"expanded_url"`
+	Indices       []int   `json:"indices"`
+	Url           string  `json:"url"`
 }
 
 type Hashtags struct {
-	Indices	[]int		`json:"indices"`
-	Text		string	`json:"text"`
+	Indices []int   `json:"indices"`
+	Text    string  `json:"text"`
 }
 
 type UserMentionsEntity struct {
-	Id					int64		`json:"id"`
-	Id_str			string	`json:"id_str"`
-	Indices			[]int		`json:"indices"`
-	Name				string	`json:"name"`
-	Screen_name string	`json:"screen_name"`
+	Id          int64    `json:"id"`
+	Id_str      string   `json:"id_str"`
+	Indices     []int    `json:"indices"`
+	Name        string   `json:"name"`
+	Screen_name string   `json:"screen_name"`
 }
 
 type MediaEntity struct {
-	Display_url						string				`json:"display_url"`
-	Expanded_url					string				`json:"expanded_url"`
-	Id										int64					`json:"id"`
-	Id_str								string				`json:"id_str"`
-	Indices								[]int					`json:"indices"`
-	Media_url							string				`json:"media_url"`
-	Media_url_https				string				`json:"media_url_https"`
-	Sizes									MediaSizes		`json:"sizes"`
-	Source_status_id			int64					`json:"source_status_id"`
-	Source_status_id_str	int64					`json:"source_status_id"`
-	Type									string				`json:"type"`
-	Url										string				`json:"url"`
+	Display_url           string        `json:"display_url"`
+	Expanded_url          string        `json:"expanded_url"`
+	Id                    int64         `json:"id"`
+	Id_str                string        `json:"id_str"`
+	Indices               []int         `json:"indices"`
+	Media_url             string        `json:"media_url"`
+	Media_url_https       string        `json:"media_url_https"`
+	Sizes                 MediaSizes    `json:"sizes"`
+	Source_status_id      int64         `json:"source_status_id"`
+	Source_status_id_str  int64         `json:"source_status_id"`
+	Type                  string        `json:"type"`
+	Url                   string        `json:"url"`
 }
 
 type MediaSizes struct {
-	Thumb	MediaSize		`json:"thumb"`
-	Large	MediaSize		`json:"large"`
-	Medium MediaSize	`json:"medium"`
-	Small	MediaSize		`json:"small"`
+	Thumb   MediaSize    `json:"thumb"`
+	Large   MediaSize    `json:"large"`
+	Medium  MediaSize    `json:"medium"`
+	Small   MediaSize    `json:"small"`
 }
 
 type MediaSize struct {
-	H				int			`json:"h"`
-	Resize	string	`json:"resize"`
-	W				int			`json:"w"`
+	H        int      `json:"h"`
+	Resize   string   `json:"resize"`
+	W        int      `json:"w"`
 }
 
 type Entities struct {
-	Hashtags			[]Hashtags						`json:"hashtags"`
-	Media					[]MediaEntity					`json:"media"`
-	Urls					[]UrlEntity						`json:"urls"`
-	User_mentions	[]UserMentionsEntity	`json:"user_mentions"`
+	Hashtags      []Hashtags             `json:"hashtags"`
+	Media         []MediaEntity          `json:"media"`
+	Urls          []UrlEntity            `json:"urls"`
+	User_mentions []UserMentionsEntity   `json:"user_mentions"`
 }
